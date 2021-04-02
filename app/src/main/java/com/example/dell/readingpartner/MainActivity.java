e09503327f102a2f68c49bd79923952331b78b24
LYHccc <1587168434@qq.com>
2021-04-02 20:00:25 +0800

    初始、登录、注册UI界面

--- app/src/main/java/com/example/dell/readingpartner/MainActivity.java
+++ app/src/main/java/com/example/dell/readingpartner/MainActivity.java
@@ -1,30 +1,38 @@
 package com.example.dell.readingpartner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import com.example.dell.readingpartner.activity.LoginActivity;
+import com.example.dell.readingpartner.activity.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.helloButton);
-        //设置监听按钮点击事件
-        button.setOnClickListener(new View.OnClickListener() {
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //得到textview实例
-                TextView hellotv = findViewById(R.id.helloTextView);
-                //弹出Toast提示按钮被点击了
-                Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_SHORT).show();
-                //读取strings.xml定义的interact_message信息并写到textview上
-                hellotv.setText(R.string.interact_message);
+            public void onClick(View view) {
+                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login);
+            }
+        });
+
+        btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(register);
             }
         });
     }
