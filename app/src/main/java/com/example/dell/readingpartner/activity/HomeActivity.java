77e6578de1448ebca7f57302988aa2edce24e8f1
LYHccc <1587168434@qq.com>
2021-04-05 15:08:54 +0800

    初始、登录、注册页面，底部导航切换

---
+++ app/src/main/java/com/example/dell/readingpartner/activity/HomeActivity.java
@@ -0,0 +1,85 @@
+package com.example.dell.readingpartner.activity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import com.example.dell.readingpartner.R;
+import com.example.dell.readingpartner.adapter.MyPagerAdapter;
+import com.example.dell.readingpartner.entity.TabEntity;
+import com.example.dell.readingpartner.fragment.DiscussFragment;
+import com.example.dell.readingpartner.fragment.HomeFragment;
+import com.example.dell.readingpartner.fragment.MyFragment;
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener, DiscussFragment.OnFragmentInteractionListener, MyFragment.OnFragmentInteractionListener{
+
+    private String[] mTitles = {"首页", "讨论", "我的"};
+    private int[] mIconUnselectIds = {
+            R.mipmap.home_unselect, R.mipmap.discuss_unselect,
+            R.mipmap.my_unselect};
+    private int[] mIconSelectIds = {
+            R.mipmap.home_selected, R.mipmap.discuss_selected,
+            R.mipmap.my_selected};
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+    private ViewPager viewPager;
+    private CommonTabLayout commonTabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        viewPager = findViewById(R.id.viewpager);
+        commonTabLayout = findViewById(R.id.commonTabLayout);
+        mFragments.add(HomeFragment.newInstance());
+        mFragments.add(DiscussFragment.newInstance());
+        mFragments.add(MyFragment.newInstance());
+        for (int i = 0; i < mTitles.length; i++) {
+            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
+        }
+
+        commonTabLayout.setTabData(mTabEntities);
+        commonTabLayout.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+                viewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onTabReselect(int position) {
+            }
+        });
+
+        viewPager.setOffscreenPageLimit(mFragments.size());
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                commonTabLayout.setCurrentTab(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), mTitles, mFragments));
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
--- app/src/main/java/com/example/dell/readingpartner/activity/LoginActivity.java
+++ app/src/main/java/com/example/dell/readingpartner/activity/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.example.dell.readingpartner.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import com.example.dell.readingpartner.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(home);
+            }
+        });
     }
 }
